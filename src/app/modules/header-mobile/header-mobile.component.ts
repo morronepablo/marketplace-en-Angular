c48adeb7b812c31c4f02121a0ef900497f76914a
morronepablo <morronepablo@gmail.com>
2021-10-14 22:27:46 -0300

    Datos dinámicos de Categorías para el menú Móvil

--- src/app/modules/header-mobile/header-mobile.component.ts
+++ src/app/modules/header-mobile/header-mobile.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Path } from '../../config';
 
+import { CategoriesService } from '../../services/categories.service';
+
+declare var jQuery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-header-mobile',
   templateUrl: './header-mobile.component.html',
@@ -9,10 +14,37 @@ import { Path } from '../../config';
 export class HeaderMobileComponent implements OnInit {
 
   path:String = Path.url;
+  categories:Object = null;
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
+
+    /*==================================================
+    Tomamos la data de las categorías
+    ==================================================*/
+
+    this.categoriesService.getData()
+    .subscribe(resp => {
+
+      this.categories = resp;
+
+      console.log("categories ", this.categories);
+
+    })
+
+    /*==================================================
+    Activamos el efecto toggle en el listado de subcategorías
+    ==================================================*/
+
+    $(document).on('click', ".sub-toggle", function() {
+
+      $(this).parent().children('ul').toggle();
+
+    })
+
+
+
   }
 
 }
