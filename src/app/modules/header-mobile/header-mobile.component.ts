5a4674c140eaa6afa9c42c1fd19ad9142eee0b84
morronepablo <morronepablo@gmail.com>
2021-10-14 23:31:40 -0300

    Datos dinámicos de Sub-categorías para el menú Móvil

--- src/app/modules/header-mobile/header-mobile.component.ts
+++ src/app/modules/header-mobile/header-mobile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Path } from '../../config';
 
 import { CategoriesService } from '../../services/categories.service';
+import { SubCategoriesService } from '../../services/sub-categories.service';
 
 declare var jQuery: any;
 declare var $: any;
@@ -15,8 +16,10 @@ export class HeaderMobileComponent implements OnInit {
 
   path:String = Path.url;
   categories:Object = null;
+  render:Boolean = true;
+  categoriesList:Array<any> = [];
 
-  constructor(private categoriesService: CategoriesService) { }
+  constructor(private categoriesService: CategoriesService, private subCategoriesService: SubCategoriesService) { }
 
   ngOnInit(): void {
 
@@ -29,7 +32,22 @@ export class HeaderMobileComponent implements OnInit {
 
       this.categories = resp;
 
-      console.log("categories ", this.categories);
+      /*==================================================
+      Recorrido por el objeto de la data de categorías
+      ==================================================*/
+
+      let i;
+
+      for(i in resp) {
+
+        /*==================================================
+        Separamos los nombres de categorías
+        ==================================================*/
+
+        this.categoriesList.push(resp[i].name)
+
+
+      }
 
     })
 
@@ -43,7 +61,76 @@ export class HeaderMobileComponent implements OnInit {
 
     })
 
+  }
 
+  /*==================================================
+  Función que nos avisa cuando finaliza el renderizado de Angular
+  ==================================================*/
+
+  callback() {
+
+    if(this.render) {
+
+      this.render = false;
+      let arraySubCategories = [];
+
+      /*==================================================
+      Separar las categorías
+      ==================================================*/
+
+      this.categoriesList.forEach(category => {
+
+        /*==================================================
+        Tomamos la colección de las sub-categorías filtrando con los nombres de categorías
+        ==================================================*/
+
+        this.subCategoriesService.getFilterData("category", category)
+        .subscribe(resp => {
+
+          /*==================================================
+          Hacemos un recorrido por la colección general de sub-categorias y
+          clasificamos de acuerdo a la categoría que correspondan
+          ==================================================*/
+
+          let i;
+
+          for(i in resp) {
+
+            arraySubCategories.push({
+
+              "category": resp[i].category,
+              "subcategory": resp[i].name,
+              "url": resp[i].url
+
+            })
+
+          }
+
+          /*==================================================
+          Recorremos el array de objetos nuevo para buscar coincidencias con las nombresde categorías
+          ==================================================*/
+
+          for(i in arraySubCategories) {
+
+            if(category == arraySubCategories[i].category) {
+
+              $(`[category='${category}']`).append(
+
+                `<li class="current-menu-item ">
+                  <a href="products/${arraySubCategories[i].url}">${arraySubCategories[i].subcategory}</a>
+                </li>`
+
+              )
+
+            }
+
+          }
+
+        })
+
+      })
+
+    }
 
   }
 
