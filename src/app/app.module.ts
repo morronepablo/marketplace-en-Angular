4429098db44ee80af6d62d55443e7096dfc78efd
morronepablo <morronepablo@gmail.com>
2021-10-14 21:59:58 -0300

    Subiendo proyecto al repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { HeaderPromotionComponent } from './modules/header-promotion/header-promotion.component';
+import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.component';
+import { NewletterComponent } from './modules/newletter/newletter.component';
+import { FooterComponent } from './modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderPromotionComponent,
+    HeaderMobileComponent,
+    NewletterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config.js
@@ -0,0 +1,18 @@
+/*==================================================
+Exportamos la ruta para tomar imágenes
+==================================================*/
+export let Path = {
+
+    url: 'http://localhost:4200/assets/'
+
+}
+
+/*==================================================
+Exportamos el endPoint de la APIREST de Firebase
+==================================================*/
+export let Api = {
+
+  url: 'https://marketplace-3c29d-default-rtdb.firebaseio.com/'
+
+}
+
